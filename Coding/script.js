f25e183af12688430e231f474638e9cbc5eaa8db
Rishi4848 <r.kalagiri@surreyschools.ca>
2025-03-05 18:25:11 -0800

    Update script.js

--- Coding/script.js
+++ Coding/script.js
@@ -11,4 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    document.querySelectorAll('.fade-in').forEach(element => {
+        observer.observe(element);
+    });
+
 });
