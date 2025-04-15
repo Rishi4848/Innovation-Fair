691858fac600f316372f2b3dcfc1e68d959b5f6f
Rishi4848 <r.kalagiri@surreyschools.ca>
2025-04-15 08:09:39 -0700

    Create service-worker.js

---
+++ Coding/service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'vitalis-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/diagnosis.html',
+    '/result.html',
+    '/Code 1.css',
+    '/script.js',
+    '/result.js',
+    '/Green & Blue Modern Connected Medical Healthcare Logo.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
